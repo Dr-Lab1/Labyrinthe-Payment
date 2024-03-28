1f7b70717fe8a97da5139321384604ed0ec67193
Dr-Lab1 <jonathankukwabantu120302@gmail.com>
2024-03-28 17:36:51 +0100

    Commenting the code for our futur documentation - 2024.03.28 at 17:36

--- src/PaymentServiceProvider.php
+++ src/PaymentServiceProvider.php
@@ -12,11 +12,16 @@ class paymentServiceProvider
 
     /**
      * From now on, this format will be used for package responses. 
-     *This array will contain the following keys: success, errors, message and data 
-     *sucess: of boolean type, which will return true if everything is OK and false if not
-     *errors: of array type, will be empty if everything is fine and will return error details if something crashes
-     *message: of type string, will return a message depending on the processing
-     *data: of type array, will return data if there is data to return, otherwise it will be empty.
+     * This array will contain the following keys: success, errors, message and data 
+     * 
+     * sucess: of boolean type, which will return true if everything is OK and false if not
+     * 
+     * errors: of array type, will be empty if everything is fine and will return error details if something crashes
+     * 
+     * message: of type string, will return a message depending on the processing
+     * 
+     * data: of type array, will return data if there is data to return, otherwise it will be empty.
+     * 
      * @var array
      * 
      */
