a409822090209e79b0bd6cab2def01bbfefe8921
Dr-Lab1 <jonathankukwabantu120302@gmail.com>
2024-03-28 16:04:32 +0100

    Updating validator - 2024.03.28 at 16:04

--- src/Validator/rules/Rules.php
+++ src/Validator/rules/Rules.php
@@ -5,22 +5,45 @@ namespace Labyrinthe\Payment\Validator\Rules;
 trait rules
 {
 
-    protected function required(array $array, string $key)
+    /**
+     * This function is used to check the existence of specific data.
+     * It returns nothing if the data exists, but returns an error if the data is missing.
+     * 
+     * @param array $array
+     * @param array $key
+     * 
+     * @return string|null
+     * 
+     */
+    protected function required(array $array, string $key): string
     {
         $valid = isset($array[$key]) ? true : false;
 
-        if (!$valid) 
-            return $key . " is required <br>";
-        
+        if (!$valid)
+            return $key . " is required";
+        else
+            return null;
     }
 
-    // The type as a string. Can be one of the following values: "boolean", "integer", "double", "string", "array", "object", "resource", "NULL", "unknown type"
-    protected function number(array $array, string $key)
+    /**
+     * This function is used to check whether the specific data sent is indeed of type number.
+     * It returns nothing if the data exists, but returns an error if the data is not a number.
+     * The type can be one of the following values: "boolean", "integer", "double"
+     * 
+     * @param array $array
+     * 
+     * @param string $key
+     * 
+     * @return string|null
+     */
+
+    protected function number(array $array, string $key): string
     {
-        $type = gettype($array[$key]);
-        if (!($type == "boolean" || $type == "integer" || $type == "double")) {
-            return $key . " is not a number <br>";
-        }
-        
+        $type = isset($array[$key]) ? gettype($array[$key]) : null;
+
+        if (!($type == "boolean" || $type == "integer" || $type == "double") || $type == null)
+            return $key . " is not a number ";
+        else
+            return null;
     }
 }
