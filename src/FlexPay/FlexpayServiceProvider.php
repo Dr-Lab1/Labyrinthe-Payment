286732b204180d528158d7b240b8ea6fd24fbdff
Dr-Lab1 <jonathankukwabantu120302@gmail.com>
2024-03-31 14:11:40 +0200

    Delete the table on mobile fx definition - 2024.03.31 at 14:11

--- src/FlexPay/FlexpayServiceProvider.php
+++ src/FlexPay/FlexpayServiceProvider.php
@@ -16,52 +16,7 @@ class FlexpayServiceProvider
      * 
      * @param array $request
      * 
-     * ------------------------------------------------------------------------------------------------------
-     *      Params      |                   Descritption                        | Example       | Required   
-     * ------------------------------------------------------------------------------------------------------
-     *  authorization   | This is the Bearer token sent by Flexpay              | Bearer xxxxx  |    YES    
-     * ------------------------------------------------------------------------------------------------------
-     *  merchant        | The merchant code is the one provided by flexpay      | "Orange"      |    YES    
-     * ------------------------------------------------------------------------------------------------------
-     *  type            | This is the type of transaction you want to carry out.| 1             |    YES    
-     *                  | In our case it's mobile. So the type will be "1".     |               |           
-     * ------------------------------------------------------------------------------------------------------
-     *  reference       | This is the transaction reference. In other words,    |               |           
-     *                  | the data that will enable the transaction to be traced| xxxxxxxxxx    |    YES    
-     *                  | on your side.                                         |               |           
-     * ------------------------------------------------------------------------------------------------------
-     *  phone           | The telephone number involved in the transaction      | 243896699032  |    YES    
-     * ------------------------------------------------------------------------------------------------------
-     *  amount          | The amount of the transaction                         | 100           |    YES    
-     * ------------------------------------------------------------------------------------------------------
-     *  currency        | This is the currency to be used in the transaction    | USD           |    YES    
-     * ------------------------------------------------------------------------------------------------------
-     *  callbackUrl     | This is the route by which the response (the final    | abcdef.com    |    YES    
-     *                  | information about the transaction) will be returned.  |               |           
-     * ------------------------------------------------------------------------------------------------------
-     *  gateway         | This is the URL that flexpay gave you to carry out    | backend.flex  |    YES    
-     *                  | these mobile transactions                             |               |           
-     * ------------------------------------------------------------------------------------------------------
-     * 
      * @return mixed
-     * 
-     * ----------------------------------------------------------------------------------------------------------------------------
-     *      Params      |                   Descritption                        | Example          
-     * ----------------------------------------------------------------------------------------------------------------------------
-     *  success         | This is the status of the request. Returns 'true' if  | true  or 1
-     *                  | everything works and 'false' if it fails.             | false or 0
-     * ----------------------------------------------------------------------------------------------------------------------------
-     *  message         | This is the message that accompanies the response to  | "Process failed"  
-     *                  | give it greater meaning                               |  
-     * ----------------------------------------------------------------------------------------------------------------------------
-     *  data            | This is an array containing the set of data returned  | [code] => 0
-     *                  | by the query                                          | [message] => Transaction envoyée avec succès.
-     *                  |                                                       | [orderNumber] => sjXMRrf98ISP243896699032 
-     * ----------------------------------------------------------------------------------------------------------------------------
-     *  errors          | A table listing all the errors encountered in the     | [errors] => Could not resolve host: beta-backend
-     *                  | request                                               |                        
-     * ----------------------------------------------------------------------------------------------------------------------------
-     * 
      */
     public static function mobile(array $array, array $options = [])
     {
