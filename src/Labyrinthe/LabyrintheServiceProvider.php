bcbd9c4ced2b685d786122777a06d40a0a437e86
Dr-Lab1 <jonathankukwabantu120302@gmail.com>
2024-05-13 14:46:40 +0200

    ... - 2024.05.13 at 1:46

--- src/Labyrinthe/LabyrintheServiceProvider.php
+++ src/Labyrinthe/LabyrintheServiceProvider.php
@@ -24,4 +24,20 @@ class LabyrintheServiceProvider
         return $mobile->betaMobile($array, $options);
     }
     
+    /**
+     * The 'checkTransaction' method is the one that facilitates rapid 
+     * checking of the payment state
+     * 
+     * It receives an array as a parameter with data such as: 
+     * orderNumber, token, gateway
+     * 
+     * @param array $array
+     * 
+     * @return mixed
+     */
+    public static function checkTransaction(array $array, array $options = [])
+    {
+        $mobile = new Labyrinthe();
+        return $mobile->checkTransaction($array, $options);
+    }
 }
